b7a18317542f4c06aa93aba9ff4053500ce7964f
@mathew_calaunan <mathew.c_bca@scientecpeople.com>
2024-05-21 15:27:37 +0800

    [refactor] import ItemForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import UserForm from './components/form/user';
+import ItemForm from './components/form/item';
 
 function App() {
   return (
     <>
       <UserForm/>
+      <ItemForm/>
     </>
   );
 }
